41879ef45390b6e6bc34e21dd7f8e39cfb04b2af
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-23 22:02:35 +0500

    added login and logout details

--- demo/urls.py
+++ demo/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('core.urls'))
 ]
 
 if settings.DEBUG:
