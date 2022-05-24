953a8c885e1918591c5f27092b187e32645a405c
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-24 12:39:13 +0500

    changed user forms

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import *
+from .forms import *
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ['username', 'email']
+
+admin.site.register(CustomUser, CustomUserAdmin)
