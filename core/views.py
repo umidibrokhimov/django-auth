9afa2082c8b51f49d3dd5c397e242903efae4da5
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-24 13:41:58 +0500

    added signin page and changed some elements

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from .forms import *
 
 class Home(TemplateView):
     template_name = 'index.html'
+
+class SignIn(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = '/accounts/login'
+    template_name = 'registration/signin.html'
--- demo/urls.py
+++ demo/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from core.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/signin', SignIn.as_view(), name='signin'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('core.urls'))
 ]
